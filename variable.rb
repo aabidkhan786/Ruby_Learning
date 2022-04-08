1d0ad03fc4991433ccd5fb92ddacf7c9a787309b
aabidkhan786 <abidkhan786@outlook.com>
2022-04-08 16:37:19 +0530

    Variables in Ruby

---
+++ variable.rb
@@ -0,0 +1,9 @@
+#In ruby we don't need to decalre a varible by typing out some name prior to it just type the name.
+
+greetings = "Hello - I'm a string variables"        #Strings Variables
+intVar = "#{255} - I'm a Integer variables"            #Integer Variables
+floatVar = "#{25.5} - I'm a Float variables"           #Float Vairables
+
+puts greetings
+puts intVar
+puts floatVar
