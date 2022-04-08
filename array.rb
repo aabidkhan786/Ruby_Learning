5b5102398ad354172e2327559a437d03952708c6
aabidkhan786 <abidkhan786@outlook.com>
2022-04-08 16:49:31 +0530

    Array initilization

---
+++ array.rb
@@ -0,0 +1,12 @@
+#In Ruby we can create an array by many ways
+
+#First way
+greetings = ["Hello", 'Holla', "Bonjour","Guten tag","salve","ola","Assalam Alaikum","Namaste"]
+puts greetings
+
+print "================================================\n"
+
+#Second Way
+greetings1 = Array.new(["Hello", 'Holla', "Bonjour","Guten tag","salve","ola","Assalam Alaikum","Namaste"])
+
+puts greetings1
