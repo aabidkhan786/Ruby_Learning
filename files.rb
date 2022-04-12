562e248aac919042e844a4daa69bf78982708b21
aabidkhan786 <abidkhan786@outlook.com>
2022-04-12 19:01:42 +0530

    Added Files Modes and Operation

--- files.rb
+++ files.rb
@@ -1,4 +1,36 @@
+=begin
+Mode |  Meaning
+-----+--------------------------------------------------------
+"r"  |  Read-only, starts at beginning of file  (default mode).
+-----+--------------------------------------------------------
+"r+" |  Read-write, starts at beginning of file.
+-----+--------------------------------------------------------
+"w"  |  Write-only, truncates existing file
+     |  to zero length or creates a new file for writing.
+-----+--------------------------------------------------------
+"w+" |  Read-write, truncates existing file to zero length
+     |  or creates a new file for reading and writing.
+-----+--------------------------------------------------------
+"a"  |  Write-only, starts at end of file if file exists,
+     |  otherwise creates a new file for writing.
+-----+--------------------------------------------------------
+"a+" |  Read-write, starts at end of file if file exists,
+     |  otherwise creates a new file for reading and
+     |  writing.
+-----+--------------------------------------------------------
+"b"  |  Binary file mode (may appear with
+     |  any of the key letters listed above).
+     |  Suppresses EOL <-> CRLF conversion on Windows. And
+     |  sets external encoding to ASCII-8BIT unless explicitly
+     |  specified.
+-----+--------------------------------------------------------
+"t"  |  Text file mode (may appear with
+     |  any of the key letters listed above except "b").
+=end
+
+
 #Reading Files in Ruby
+#First Method
 File.open("methods.txt","r") do |file|      #r : stands for reading out the files
     
     #To print out the Singel line of the docuemnt we use, .readlines()
@@ -15,3 +47,11 @@ File.open("methods.txt","r") do |file|      #r : stands for reading out the file
         puts line
     end
 end
+
+#Second Method : In this method one things to remmeber that we need to close the file opening.
+file = File.open("methods.txt","r")
+puts file.read
+file.close()
+
+#================================================================================
+#Writing Files in Ruby
