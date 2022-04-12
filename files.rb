018c897385730a088c7a31bfe69db06ebe7be0e2
aabidkhan786 <abidkhan786@outlook.com>
2022-04-12 18:19:56 +0530

    Different ways of reading out the files

--- files.rb
+++ files.rb
@@ -1,4 +1,17 @@
 #Reading Files in Ruby
 File.open("methods.txt","r") do |file|      #r : stands for reading out the files
-    puts file.read()
+    
+    #To print out the Singel line of the docuemnt we use, .readlines()
+    # puts file.readlines()
+
+    #To print out the whole document we use .read()
+    # puts file.read()
+
+    #To print out the Singel Character of the docuemnt we use, .readchar()
+    # puts file.readchar()
+
+    #Alternative of .read() we have the option of looping readline()
+    for line in file.readlines()
+        puts line
+    end
 end
